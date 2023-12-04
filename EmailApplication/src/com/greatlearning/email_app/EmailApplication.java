0be4dbf938b2d559a46de3ec3ada5418b5d623c3
Vinay Srinivasan <4380223+SriVinayaka@users.noreply.github.com>
2023-12-04 17:24:24 +0530

    04DEC2023
    04DEC2023

--- EmailApplication/src/com/greatlearning/email_app/EmailApplication.java
+++ EmailApplication/src/com/greatlearning/email_app/EmailApplication.java
@@ -10,19 +10,36 @@ public class EmailApplication {
 	{
 		Scanner in = new Scanner(System.in);
 		
+		int department = 0;
+		
 		System.out.println("Enter the Employee First Name:");
 		String firstName = in.nextLine();
 		
 		System.out.println("Enter the Employee Last Name:");
 		String lastName = in.nextLine();
 		
-		System.out.println("Enter the Employee Department:");
-		System.out.println("1 - Technical");
-		System.out.println("2 - Admin");
-		System.out.println("3 - Human Reource");
-		System.out.println("4 - Legal");
-		int department = in.nextInt();
+		boolean myflag = true;
 		
+		while(myflag)
+		{
+			System.out.println("Enter the Employee Department:");
+			System.out.println("1 - Technical");
+			System.out.println("2 - Admin");
+			System.out.println("3 - Human Reource");
+			System.out.println("4 - Legal");
+			department = in.nextInt();
+			
+			if((department >= 1) && (department <= 4))
+			{
+				myflag = false;
+				break;
+			}
+			else
+			{
+				System.out.println("\nPlease Enter Correct Employee Department\n");
+				continue;
+			}
+		}
 		String emailaddress = new CredentialsManagerServiceImpl().generateEmailAddress(firstName, lastName, department);
 		
 		String password = new CredentialsManagerServiceImpl().generatePassword();
